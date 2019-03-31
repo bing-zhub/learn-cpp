86765822842a28b59550efda83bf00b59eb0e80d
bing.zhub <bing.zhub@gmail.com>
2019-03-31 16:35:23 +0800

    update add and sub of big number

--- day08/bign.cpp
+++ day08/bign.cpp
@@ -39,11 +39,51 @@ int greaterThan(bign a, bign b){
     }
 }
 
+bign add(bign a, bign b){
+    bign c;
+    int carry = 0;
+    for(int i = 0; i < max(a.len, b.len); i++){
+        int temp = a.d[i] + b.d[i] + carry;
+        c.d[c.len++] = temp%10;
+        carry = temp/10;
+    }
+
+    if(carry!=0)
+        c.d[c.len++] = carry;
+    return c;
+}
+
+bign sub(bign a, bign b){
+    bign c;
+    for(int i = 0; i < max(a.len, b.len); i++){
+        if(a.d[i] < b.d[i]){
+            a.d[i+1]--;
+            a.d[i] += 10;
+        }
+        c.d[c.len++] = a.d[i] - b.d[i];
+    }
+    while(c.len - 1>=1 && c.d[c.len -1] ==0) // 去除高位的0, 并且保证至少有一个
+        c.len --;
+    
+    return c;
+}
+
 int main(){
     char str1[] = "123456";
     char str2[] = "234567";
     bign a = toBign(str1);
     bign b = toBign(str2);
-    cout << greaterThan(b,a);
+    cout << greaterThan(b,a) << endl;
+
+    bign c = add(a,b);
+    for(int i = c.len-1; i >=0; i--){
+        cout << c.d[i];
+    }
+    cout << endl;
+
+    bign d = sub(b,a);
+    for(int i = d.len-1; i >=0; i--){
+        cout << d.d[i];
+    }
     return 0;
 }
---
+++ day08/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+while(c.len - 1>=1 && c.d[c.len -1] ==0)
+    //     c.len --;
