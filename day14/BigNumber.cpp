b43fa27f1cad0ba021e40f08e303a7c55d9f7ec2
bing.zhub <bing.zhub@gmail.com>
2019-05-25 23:39:41 +0800

    添加高精度计算 前缀和 差分

--- day14/BigNumber.cpp
+++ day14/BigNumber.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -36,6 +37,7 @@ vector<int> sub(vector<int> a, vector<int> b){
     t = a[i] - t;
     if(i < b.size()) t -= b[i];
     c.push_back((t+10)%10);
+    // 借位 1 不借位 0
     if(t < 0) t = 1;
     else t = 0; 
   }
@@ -44,25 +46,69 @@ vector<int> sub(vector<int> a, vector<int> b){
   return c;
 }
 
-int main(){
-  string a, b;
-  vector<int> A,B;
+vector<int> mul(vector<int> &a, int b){
+  vector<int> c;
+  int t = 0; // 进位
+  for(int i = 0; i < a.size() || t; i++){
+    if(i < a.size()) t += a[i] * b;
+    c.push_back(t % 10);
+    t /= 10;
+  }
+  return c;
+}
 
-  cin >> a >> b; // a = "123456"
-  for(int i = a.size() - 1; i>=0; i--) A.push_back(a[i] - '0'); // a = {6,5,4,3,2,1}
-  for(int i = b.size() - 1; i>=0; i--) B.push_back(b[i] - '0');
-
-  auto C = add(A, B);
-
-  for(int i = C.size() - 1; i >=0 ; i--) printf("%d", C[i]);
-  cout << endl;
-  if(cmp(A, B)){
-    auto C = sub(A,B);
-    for(int i = C.size() - 1; i >=0 ; i--) printf("%d", C[i]);
-  } else {
-    auto C = sub(B,A);
-    for(int i = C.size() - 1; i >=0 ; i--) printf("%d", C[i]);
+// C = A / b 余 r
+vector<int> div(vector<int> &a, int b, int &r){
+  vector<int> c;
+  r = 0;
+  for(int i = a.size() - 1; i >=0 ; i--){
+    r = r * 10 + a[i];
+    c.push_back(r / b);
+    r %= b;
   }
 
+  reverse(c.begin(), c.end());
+  while(c.size() > 1 &&  c.back() == 0) c.pop_back();
+  return c;
+}
+
+int main(){
+  // string a, b;
+  // vector<int> A,B;
+
+  // cin >> a >> b; // a = "123456"
+  // for(int i = a.size() - 1; i>=0; i--) A.push_back(a[i] - '0'); // a = {6,5,4,3,2,1}
+  // for(int i = b.size() - 1; i>=0; i--) B.push_back(b[i] - '0');
+
+  // auto C = add(A, B);
+
+  // for(int i = C.size() - 1; i >=0 ; i--) printf("%d", C[i]);
+  // cout << endl;
+  // if(cmp(A, B)){
+  //   auto C = sub(A,B);
+  //   for(int i = C.size() - 1; i >=0 ; i--) printf("%d", C[i]);
+  // } else {
+  //   auto C = sub(B,A);
+  //   for(int i = C.size() - 1; i >=0 ; i--) printf("%d", C[i]);
+  // }
+
+
+  // string a;
+  // int b;
+  // cin >> a >> b;
+  // vector<int> A;
+  // for(int i = a.size() - 1; i >= 0; i--) A.push_back(a[i] - '0'); 
+  // auto C = mul(A, b);
+  // for(int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]);
+  
+  string a;
+  int b;
+  cin >> a >> b;
+  vector<int> A;
+  for(int i = a.size() - 1; i >= 0; i--) A.push_back(a[i] - '0'); 
+  int r;
+  auto C = div(A, b, r);
+  for(int i = C.size() - 1; i >= 0; i--) printf("%d", C[i]);
+  cout << endl << r << endl;
   return 0;
 }
