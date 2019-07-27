6ebcc40b5f34b402637a8b79305c34ecba6830e1
bing.zhub <bing.zhub@gmail.com>
2019-07-27 10:41:50 +0800

    update PAT a1005

--- PAT_Solution/Advanced/1005.cpp
+++ PAT_Solution/Advanced/1005.cpp
@@ -1,18 +1,27 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-	string in;
-	cin >> in;
-	int sum = 0;
-	for(int i = 0; i < in.length(); i++)
-		sum += in[i] - '0';
-	
-	in = to_string(sum);
-	string mapping[10] = {"zero","one", "two","three","four","five","six","seven","eight","nine"};
-	cout << mapping[in[0] - '0'];
-	for(int i = 1; i < in.length(); i++){
-		cout << " " << mapping[in[i]-'0']; 
-	}
-	return 0;
+string map[10] = {"zero", "one", "two",   "three", "four",
+                  "five", "six", "seven", "eight", "nine"};
+int sum;
+
+int main() {
+  vector<int> res;
+  string in;
+  cin >> in;
+  for (int i = in.size() - 1; i >= 0; i--)
+    sum += in[i] - '0';
+  while (sum > 0)
+    res.push_back(sum % 10), sum /= 10;
+  if (in != "0") {
+    reverse(res.begin(), res.end());
+    cout << map[res[0]];
+    for (int i = 1; i < res.size(); i++)
+      cout << " " << map[res[i]];
+  } else
+    cout << "zero";
+
+  return 0;
 }
---
+++ PAT_Solution/Advanced/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+	// reverse(res.begin(), res.end());
+	// cout << map[res[0]];
+	// for(int i = 1; i < res.size(); i ++){
+	// 	cout << " " << map[res[i]];
+	// }
